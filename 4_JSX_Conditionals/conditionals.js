bc4e13382598184e1b99ee335035fce49243da43
krishnatandon1208 <krishnatandon.1208@gmail.com>
2020-05-13 07:56:08 +0530

    JSX Basics

---
+++ 4_JSX_Conditionals/conditionals.js
@@ -0,0 +1,38 @@
+function getRandomNumber() {
+    return(
+        (Math.floor(Math.random() * 10))
+    );
+}
+
+class NumPicker extends React.Component {
+    render() {
+        //Match the conditions
+        const num = getRandomNumber();
+        let newNum;
+        if(num === 7){
+            newNum = "Congratulations"
+            
+        } else if (num === 0) {
+            newNum = "U got 0"
+        } else  {
+            newNum = "unlucky"
+        }
+        return(
+            <div>
+                <h1>Your random number {num}</h1>
+                <h3>
+                    {newNum}
+                    {/* {
+                     num ===7 
+                     ?<img src="success.gif" alt="Success" />
+                     : null
+                    } */}
+
+                    {num ===7  && <img src="success.gif" alt="Success" />}
+                </h3>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<NumPicker />, document.getElementById("root"));
